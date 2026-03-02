34e97c96970b4a68b539cdb4dbd189329891ca36
privetipoka80-pixel <privetipoka80@gmail.com>
2026-03-02 16:21:20 +0300

    2

--- main.py
+++ main.py
@@ -104,5 +104,26 @@ def promotion_image():
         '''
 
 
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def results(nickname, level, rating):
+    return f'''
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet"
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                    crossorigin="anonymous">
+                    <h1>Результаты отбора</h1>
+                    <h1>Претенденты на участие в миссии {nickname}</h1>
+           <div class="alert alert-success" role="alert">
+                    Поздравялем! ваш рейтинг после {level} отбора
+                    </div>
+                    Составляет {rating}!
+                    <div class="alert alert-warning" role="alert">
+                    Желаем удачи!
+                    </div>
+        '''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
