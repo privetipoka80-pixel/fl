9c86cbeafd048268f87076e759d29a1a95b5d39b
privetipoka80-pixel <privetipoka80@gmail.com>
2026-03-02 17:10:10 +0300

    3

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, url_for
-
+from facts import facts
 app = Flask(__name__)
 text = """Человечество вырастает из детства.
 
@@ -39,37 +39,47 @@ def image_mars():
 
 @app.route('/choice/<choicePlanet>')
 def choice(choicePlanet):
-    return f'''<!doctype html>
-                <html lang="en">
-                  <head>
-                    <meta charset="utf-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                    <link rel="stylesheet"
-                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-                    crossorigin="anonymous">
-                    <h1>Мое предложение: {choicePlanet}</h1>
-                    <h5>Эта планета близка к земле;</h5>
-                  </head>
-                  <body>
-                    <h1>Привет, Яндекс!</h1>
-                    <div class="alert alert-success" role="alert">
-                      А мы тут компонентами Bootstrap балуемся
-                    </div>
-                    <div class="alert alert-primary" role="alert">
-                    На ней много необходимых ресурсов;
-                    </div>
-                    <div class="alert alert-warning" role="alert">
-                    На ней есть вода и атмосфера;
-                    </div>
-                    <div class="alert alert-dark" role="alert">
-                    На ней есть небольшое магнитное поле;
-                    </div>
-                    <div class="alert alert-danger" role="alert">
-                    Наконец, она просто красива!
-                    </div>
-                  </body>
-                </html>'''
+    choicePlanet = choicePlanet.lower()
+    if choicePlanet in facts:
+        return f'''<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                        <link rel="stylesheet"
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                        crossorigin="anonymous">
+                        <h1>Мое предложение: {choicePlanet.capitalize()}</h1>
+                        <div class="alert alert-light" role="alert">
+                        {facts[choicePlanet][0]}
+                        </div>
+                      </head>
+                      <body>
+                        <div class="alert alert-primary" role="alert">
+                        {facts[choicePlanet][1]}
+                        </div>
+                        <div class="alert alert-warning" role="alert">
+                        {facts[choicePlanet][2]}
+                        </div>
+                        <div class="alert alert-dark" role="alert">
+                        {facts[choicePlanet][3]}
+                        </div>
+                        <div class="alert alert-danger" role="alert">
+                        {facts[choicePlanet][4]}
+                        </div>
+                      </body>
+                    </html>'''
+    return """<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                        <link rel="stylesheet"
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                        crossorigin="anonymous">
+                        <h1>КРИТИЧЕСКАЯ ОШИБКА!!!!!!!!!!!</h1>"""
 
 
 @app.route('/promotion_image')
